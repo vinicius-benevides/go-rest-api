09d3a4c8b19d027b0dc3d606fd8175acb4e941b3
Vinícius Benevides <viniciusbenevidesdasilva16@gmail.com>
2026-03-26 11:04:02 -0300

    feat: add list and create events endpoints

--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/vinicius-benevides/go-rest-api/models"
 )
 
 func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Hello, World!",
+	events := models.GetAllEvents()
+
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	if err := ctx.ShouldBindJSON(&event); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": fmt.Sprintf("Could not parse event data: %v", err),
+		})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "Event created",
+		"event":   event,
 	})
 }
