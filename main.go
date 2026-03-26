6cc58452a6fc01d56c89d2534ae09f472d10d9c5
Vinícius Benevides <viniciusbenevidesdasilva16@gmail.com>
2026-03-26 10:40:23 -0300

    feat: create base project with gin dependency

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080")
+}
+
+func getEvents(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Hello, World!",
+	})
+}
