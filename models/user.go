362c0624eaeb463591a37fb5a153d08b18b09dd8
Vinícius Benevides <viniciusbenevidesdasilva16@gmail.com>
2026-03-26 15:06:47 -0300

    feat: hash password before saving it

--- models/user.go
+++ models/user.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 
 	"github.com/vinicius-benevides/go-rest-api/db"
+	"github.com/vinicius-benevides/go-rest-api/utils"
 )
 
 type User struct {
 	ID       int64
 	Email    string `binding:"required"`
-	Password string `binding:"required"`
+	Password string `json:",omitempty" binding:"required"`
 }
 
 func (u *User) Save() error {
@@ -24,6 +25,11 @@ func (u *User) Save() error {
 	}
 	defer stmt.Close()
 
+	u.Password, err = utils.HashPassword(u.Password)
+	if err != nil {
+		return fmt.Errorf("Could not hash user password: %v", err)
+	}
+
 	result, err := stmt.Exec(u.Email, u.Password)
 	if err != nil {
 		return fmt.Errorf("Could not execute statement for saving user: %v", err)
@@ -33,6 +39,8 @@ func (u *User) Save() error {
 	if err != nil {
 		return fmt.Errorf("Could not get last inserted id for saved user: %v", err)
 	}
+
+	u.Password = ""
 
 	return nil
 }
